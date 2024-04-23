9f36fbf62045061a147407871ccef4c18e81744b
Joel Fankam <37833979+joekrom@users.noreply.github.com>
2024-04-23 13:04:30 +0200

    Update notifier.py to handle multiple emails

--- notifier.py
+++ notifier.py
@@ -1,9 +1,11 @@
 import smtplib, ssl
 import os
+import re
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
+# for validating an Email
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
 smtp_port = os.environ.get("INPUT_SMTP_PORT")
 smtp_address = os.environ.get("INPUT_SMTP_ADDRESS")
@@ -15,45 +17,58 @@ body = os.environ.get("INPUT_BODY")
 sender_email = os.environ.get("INPUT_FROM")
 ssl_tls = os.environ.get("INPUT_SSL")
 
-
+def set_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
+        print(f'{name}={value}', file=f)
+        
+def validate_mail(email):
+    # pass the regula expression
+    # and the string into a fullmathc method
+    try:
+        re.fullmatch(regex, email)
+        return true
+    except:
+        value = email + " {0}"
+        set_output("status",value.format("is not valid") )
+        return false
 def send_email():
     # Check if any of the environment variables are empty or None
     if None in (smtp_port, smtp_address, sender_email_auth, sender_password_auth, receiver_email, sender_email) or '' in (smtp_port, smtp_address, sender_email_auth, sender_password_auth, receiver_email, sender_email):
              # writing to the buffer
             output = "One or more required environment variables are empty or not set."
             #io.write_to_output({"status": f"Error: {output}"})
+            set_output("status", "environment variables are empty or None")
             raise ValueError(output)
-
+        
     message = MIMEMultipart("alternative")
     message["Subject"] = subject
     message["From"] = sender_email
-    message["To"] = receiver_email
-
-    text = """\
-        default message from action
-    """
     text = body
-
     part1 = MIMEText(text, "plain")
     message.attach(part1)
-    # Create a secure SSL context
-    context = ssl.create_default_context()
-
-    # Try to log  to server and send email
-    try:
-      server = smtplib.SMTP(smtp_address, int(smtp_port))
-      server.ehlo()
-      if ssl_tls == 'true':
-          server.starttls(context=context)
-      server.ehlo()
-      server.login(sender_email_auth, sender_password_auth)
-      server.sendmail(sender_email, receiver_email, message.as_string())
-    except Exception as e:
-        output = "One or more required environment variables are empty or not set."
-        #io.write_to_output({"status": f"Error: {output}"})
-        print(e)
-    finally:
-      server.quit()
+    
+    if (len(receiver_email.split(",")) > 1):
+        emails = receiver_email.split(",")
+        for email_addr in emails:
+            message["To"] = email_addr
+            # Create a secure SSL context
+            context = ssl.create_default_context()
+        
+            # Try to log  to server and send email
+            try:
+              server = smtplib.SMTP(smtp_address, int(smtp_port))
+              server.ehlo()
+              if ssl_tls == 'true':
+                  server.starttls(context=context)
+              server.ehlo()
+              server.login(sender_email_auth, sender_password_auth)
+              server.sendmail(sender_email, email_addr, message.as_string())
+            except Exception as e:
+                output = "One or more required environment variables are empty or not set."
+                set_output("status", output)
+                print(e)
+            finally:
+              server.quit()
 
 
 if __name__ == "__main__":
